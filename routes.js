6891980a5bb4b6f96f9caf7a41e891a04601c0fa
David Samamé <94189074+JDavidSPX@users.noreply.github.com>
2023-11-23 16:29:26 -0500

    Update routes.js

--- routes.js
+++ routes.js
@@ -4,14 +4,14 @@ const btnDocumentation = document.querySelector("#btnDocumentation");
 const btnInformation = document.querySelector("#btnInformation");
 
 btnHome.addEventListener("click", function () {
-  location.href = "/";
+  location.href = "CAF02/";
 });
 btnComparison.addEventListener("click", function () {
-  location.href = "/comparison.html";
+  location.href = "CAF02/comparison.html";
 });
 btnDocumentation.addEventListener("click", function () {
-  location.href = "/documentation.html";
+  location.href = "CAF02/documentation.html";
 });
 btnInformation.addEventListener("click", function () {
-  location.href = "/information.html";
+  location.href = "CAF02/information.html";
 });
